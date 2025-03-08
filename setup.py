1f8863d44c43072a9fb00dc1765f43ee75e9276c
necococo <kmochidadihcomk@gmail.com>
2025-03-08 22:34:22 +0900

    del torch numpy

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "torch",
-        "numpy",
         "sentencepiece",
         "pywavelets",
         "datasets",
